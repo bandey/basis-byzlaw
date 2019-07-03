6d98283c231040c9e580a2c1b08b379a4435e4b1
bandey <origbandey@gmail.com>
2019-07-03 21:52:36 +0300

    Wire up logger as express middleware

--- server/server-express.js
+++ server/server-express.js
@@ -6,23 +6,30 @@ const ejsLayouts = require('express-ejs-layouts');
 
 const serverExpress = express();
 
+serverExpress.disable('x-powered-by');
+
+// Wire up logger and set field locals
+serverExpress.use((req, res, next) => {
+  const now = logger.getNow();
+  logger.log('>> %s %s from %s\n%s', now, req.method,
+    req.connection.remoteAddress.replace(/.*\:/, ''), req.url);
+  res.locals = {now};
+  return next();
+});
+
 // Configure views
 serverExpress.set('views', path.join(__dirname, '../views'));
 serverExpress.set('view engine', 'ejs');
 serverExpress.set('layout', 'layout-main');
 serverExpress.use(ejsLayouts);
 
-serverExpress.disable('x-powered-by');
 serverExpress.get('/', (req, res) => {
-  logger.log('>> %s %s from %s\n%s', logger.getNow(), req.method,
-      req.connection.remoteAddress.replace(/.*\:/, ''), req.url);
   return res.render('welcome');
 });
 
 // Catch 404 error
 serverExpress.use((req, res, next) => {
-  logger.log('>> %s %s from %s\n%s\nWRONG Route', logger.getNow(), req.method,
-    req.connection.remoteAddress.replace(/.*\:/, ''), req.url);
+  logger.log('WRONG Route');
   res.status(404);
   return res.render('error', {
     layout: 'layout-error',
@@ -32,16 +39,14 @@ serverExpress.use((req, res, next) => {
 
 // Error handler
 serverExpress.use((err, req, res, next) => {
-  let objErr = {message: err.message};
-  objErr.status = err.status || 500;
-  if (config.get('nodeEnv') === 'development') {
-    objErr.stack = err.stack;
-  }
+  let objErr = {
+    status: err.status || 500,
+    message: err.message,
+    stack: (config.get('nodeEnv') === 'development') ? err.stack : null,
+  };
 
-  const now = logger.getNow();
-  logger.log('>> %s %s from %s\n%s\nERR express server %d', now, req.method,
-    req.connection.remoteAddress.replace(/.*\:/, ''), req.url, objErr.status);
-  logger.error('>> %s %s\n%s\n%s', now, err.name, err.message, err.stack);
+  logger.log('ERR express server %d', objErr.status);
+  logger.error('>> %s %s\n%s\n%s', res.locals.now, err.name, err.message, err.stack);
 
   res.status(objErr.status);
   return res.render('error', {
