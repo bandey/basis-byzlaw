2be7a78ce284f0a93df8584714c2f1206987b307
bandey <origbandey@gmail.com>
2019-07-03 21:29:36 +0300

    Add ejs view engine with layouts

--- server/server-express.js
+++ server/server-express.js
@@ -1,12 +1,20 @@
 const logger = require('../tooling/logger.js')('server:express');
 const express = require('express');
+const path = require('path');
+const ejsLayouts = require('express-ejs-layouts');
 
 const serverExpress = express();
 
+serverExpress.set('views', path.join(__dirname, '../views'));
+serverExpress.set('view engine', 'ejs');
+serverExpress.set('layout', 'layout-main');
+serverExpress.use(ejsLayouts);
+
+serverExpress.disable('x-powered-by');
 serverExpress.get('/', (req, res) => {
   logger.log('>> %s %s from %s\n%s', logger.getNow(), req.method,
       req.connection.remoteAddress.replace(/.*\:/, ''), req.url);
-  return res.send('Hello world!');
+  return res.render('welcome.ejs');
 });
 
 module.exports = serverExpress;
